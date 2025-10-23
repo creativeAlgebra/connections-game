fe8b7705eaf6add2dc9caca0803873282624e09d
Randal Garcia <84541455+creativeAlgebra@users.noreply.github.com>
2025-10-23 14:44:04 -0700

    Add hintJustRevealed state to App component

--- src/App.jsx
+++ src/App.jsx
@@ -87,6 +87,7 @@ const ConnectionsGame = () => {
   const [message, setMessage] = useState("");
   const [gameWon, setGameWon] = useState(false);
   const [mistakes, setMistakes] = useState(0);
+  const [hintJustRevealed, setHintJustRevealed] = useState(false);
 
   useEffect(() => {
     initializeGame();
@@ -112,6 +113,7 @@ const ConnectionsGame = () => {
     setMessage("");
     setGameWon(false);
     setMistakes(0);
+    setHintJustRevealed(false);
   };
 
   const toggleSelect = (index) => {
